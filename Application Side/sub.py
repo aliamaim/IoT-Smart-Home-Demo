7025cccc07a5ec3377612c153142b0fd027eb60c
aliamaim <ali.amaim@yahoo.com>
2019-07-09 15:22:54 +0200

    Create sub.py

---
+++ Application Side/sub.py
@@ -0,0 +1,19 @@
+import paho.mqtt.client as mqtt
+
+broker_url = "iot.eclipse.org"
+broker_port = 1883
+
+def on_connect(client, userdata, flags, rc):
+   print("Connected With Result Code "+rc)
+
+def on_message(client, userdata, message):
+   print("Message Recieved: "+message.payload.decode())
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(broker_url, broker_port)
+
+client.subscribe("g2k/esp/test", qos=1)
+
+client.loop_forever()
