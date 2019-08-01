fb6f76b6064e9c54ad4d58d7186256cf0a48e320
aliamaim <ali.amaim@yahoo.com>
2019-08-01 10:37:21 +0200

    Delete pub.py

--- Application Side (Raspberry Pi 3)/pub.py
+++
@@ -1,9 +0,0 @@
-import paho.mqtt.client as mqtt
-
-broker_url = "iot.eclipse.org"
-broker_port = 1883
-
-client = mqtt.Client()
-client.connect(broker_url, broker_port)
-
-client.publish(topic="g2k/esp/test", payload="Hello from Python Script", qos=1, retain=False)
