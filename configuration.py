a22258864a65294d0b79d5a74694c397314e3ea4
aliamaim <ali.amaim@yahoo.com>
2019-08-04 11:31:22 +0200

    Add files via upload

--- configuration.py
+++ configuration.py
@@ -3,19 +3,20 @@ import csv
 from tkinter import *
 
 
-houseid = "a000"
-
+houseid = "a000"  # To be modified so that it's configured by user/program and saved in the config.csv
+# standard frame width & height
 frame_width = 250
 frame_height = 650
 
 
+# Set up the configuration file according to the user preferences for future use
 def initialize_first_time():
     with open("config.csv", 'w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
         frames_number = convert_input_to_int("How many rooms is your home?: ")
         for i in range(frames_number):
             frame_name = input("What is the name of this room?: ")
-            frame_code = frame_name.lower().split()[0]  # If name is Living Room code will be living
+            frame_code = frame_name.lower().split()[0]  # If name is Living Room code will be --> living
             devices_number = convert_input_to_int("How many digital(ON/OFF) devices you wish to control?: ")
             frame_info = [frame_name, frame_code, devices_number]
             for j in range(devices_number):
@@ -23,6 +24,7 @@ def initialize_first_time():
             csv_writer.writerow(frame_info)
 
 
+# Initialize the application from configuration file (already configured)
 def initialize_from_save():
     with open("C:/Users/Ali.Amr/PycharmProjects/Smart_Home/config.csv", 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -31,7 +33,7 @@ def initialize_from_save():
         for row in csv_reader:
             frame_name = row[0]
             frame_code = row[1]
-            devices_number = row[2]
+            devices_number = row[2]  # Not used yet
             devices_names = []
             for device_name in row[3:]:
                 devices_names.append(device_name)
@@ -42,6 +44,7 @@ def initialize_from_save():
         return list_of_frames
 
 
+# Try to convert text to int if it's not an int raise a ValueError
 def convert_input_to_int(prompt_text):
     try:
         number = int(input(prompt_text))
